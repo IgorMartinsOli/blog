b7826d9b824a7d1c248f1180da7e5729c9e81452
Igor_ <igrmartoli@gmail.com>
2022-04-17 19:44:03 -0300

    starting the project foundations

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connect = require('./Database/database.js');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+connect
+    .authenticate()
+    .then(() => {
+        console.log('Conexão com BD foi um sucesso');
+    }).catch((err) => {
+        console.log('Erro: ' + err.message);
+    });
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(8080, () => {
+    console.log('Server listening on port 8080');
+});
