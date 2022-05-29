9256b97c2e6c225bc7f41fe22c343b4af0c055a1
Igor_ <igrmartoli@gmail.com>
2022-05-29 20:30:23 -0300

    add selection of categorye to the creat of article

--- Articles/articlesController.js
+++ Articles/articlesController.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Categorie = require('../Categories/Categorie');
 
 router.get('/artiles', (req, res) => {
     res.send('Rota em Articles')
 });
 
-
 router.get('/admin/articles/new', (req, res) => {
-    res.render('admin/articles/new');
+    Categorie.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories})
+    })
+    
 });
+
+
 
 module.exports = router;
