5db3fb32666c9e79f6f4c9d2f112ca8c550fcf0d
Igor_ <igrmartoli@gmail.com>
2022-06-01 19:05:10 -0300

    finish crud article

--- Articles/articlesController.js
+++ Articles/articlesController.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const Categorie = require('../Categories/Categorie');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/artiles', (req, res) => {
-    res.send('Rota em Articles')
+router.get('/admin/articles', (req, res) => {
+    Article.findAll({
+        include:[{model:Categorie, required: true}]
+    }).then(articles => {
+        res.render('admin/articles/index', {articles: articles});
+    })
 });
 
 router.get('/admin/articles/new', (req, res) => {
     Categorie.findAll().then(categories => {
         res.render('admin/articles/new', {categories: categories})
     })
-    
 });
 
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
 
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post('/articles/delete', (req, res) => {
+    let id = req.body.id;
+    if(id != null && id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where: {id: id},
+            }).then(() => {
+                res.redirect('/admin/articles');
+            });
+        }else{//diferente de numero
+            res.redirect('/admin/articles');
+        }
+    }else{//null ou undefinido
+        res.redirect('/admin/articles');
+    }
+});
 
 module.exports = router;
--- Database/database.js
+++ Database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const connect = new Sequelize(`guiapress`, `root`, `123456`, {
     host: 'localhost',
     dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connect;
